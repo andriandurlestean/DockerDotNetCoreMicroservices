9ce50f54d8731bfbbd54f1ae475fde6a02a70ff1
Andrian Durlestean <andrian.durlestean@wirtek.com>
2018-10-08 11:02:42 +0300

    - added untit tests for jenkins pipeline

---
+++ TestWebApi/UnitTests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void OMG_TEST_ME()
+        {
+            Assert.True(true);
+        }
+    }
+}
